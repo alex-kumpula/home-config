f0051596877961234c254ef4bf7377d0f385b38d
Alex Kumpula <alex.kumpula01@gmail.com>
2025-08-24 00:56:05 -0600

    add service

--- modules/applications/keymapper.nix
+++ modules/applications/keymapper.nix
@@ -3,4 +3,20 @@
   home.packages = with pkgs; [
     keymapper
   ];
+
+  systemd.user.services.keymapperd = {
+    Unit = {
+      Description = "Keymapper Daemon";
+      After = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      ExecStart = "${pkgs.keymapper}/bin/keymapperd";
+      Restart = "always";
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }
