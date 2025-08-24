b9dca642e63158e196aed08a8ea28519945774a9
Alex Kumpula <alex.kumpula01@gmail.com>
2025-08-23 19:05:08 -0600

    Update steam

--- modules/applications/steam.nix
+++ modules/applications/steam.nix
@@ -4,6 +4,11 @@
     steam
   ];
 
+  # Fixes weird issues with drop-down menus being offset in the wrong spot
+  home.sessionVariables = {
+    STEAM_USE_NATIVE_WAYLAND = "1";
+  };
+
   # Optionally, you can enable steam-related programs or services here
   # (Home Manager doesn't manage Steam itself, but this ensures the steam binary is installed)
 }
