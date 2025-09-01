0c3a8bab3cbdc032c92f444412cc6b919e842e79
Alex Kumpula <alex.kumpula01@gmail.com>
2025-09-01 02:03:45 -0600

    add

--- modules/applications/bash/default.nix
+++ modules/applications/bash/default.nix
@@ -9,6 +9,15 @@
     bashrcExtra = ''
       # Custom stuff here
       alias ll='ls -lah'
+
+      # Yazi shell wrapper (see the Yazi website Getting Started page)
+      function y() {
+        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+        yazi "$@" --cwd-file="$tmp"
+        IFS= read -r -d '\0' cwd < "$tmp"
+        [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+        rm -f -- "$tmp"
+      }
     '';
 
     # Appended to ~/.bash_profile
