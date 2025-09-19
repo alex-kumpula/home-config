5ae907942f95656845bc82c30618cbafe044841e
Alex Kumpula <alex.kumpula01@gmail.com>
2025-09-19 12:13:45 -0600

    test

--- modules/applications/python.nix
+++ modules/applications/python.nix
@@ -1,10 +1,11 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    python3
-    python3Packages.pip
-    python3Packages.virtualenv
-    python3Packages.virtualenvwrapper
+    # python3
+    # python3Packages.pip
+    # python3Packages.virtualenv
+    # python3Packages.virtualenvwrapper
+
 
     (python311.withPackages (ps: with ps; [
       numpy # these two are
