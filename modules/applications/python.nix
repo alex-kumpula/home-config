9e44416b3ba2fb4c4f51445871e236c4cad4a89f
Alex Kumpula <alex.kumpula01@gmail.com>
2025-09-19 12:05:09 -0600

    add python deps

--- modules/applications/python.nix
+++ modules/applications/python.nix
@@ -5,5 +5,14 @@
     python3Packages.pip
     python3Packages.virtualenv
     python3Packages.virtualenvwrapper
+
+    (python311.withPackages (ps: with ps; [
+      numpy # these two are
+      scipy # probably redundant to pandas
+      jupyterlab
+      pandas
+      statsmodels
+      scikitlearn
+    ]))
   ];
 }
