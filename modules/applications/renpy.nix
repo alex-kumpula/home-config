eff1b4cef09e41f12c2cedddca89be9a8d6b5b7d
Alex Kumpula <alex.kumpula01@gmail.com>
2025-09-27 15:32:51 -0600

    test

--- modules/applications/renpy.nix
+++ modules/applications/renpy.nix
@@ -3,4 +3,13 @@
   home.packages = with pkgs; [
     renpy
   ];
+
+  xdg.desktopEntries.org-protocol = {
+    name = "renpy";
+    exec = "renpy";
+    terminal = false;
+    type = "Application";
+    categories = ["System"];
+    mimeType = ["x-scheme-handler/org-protocol"];
+  };
 }
