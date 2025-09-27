bf5ff5afd2c185df99d7c99a3e959935b4b810a7
Alex Kumpula <alex.kumpula01@gmail.com>
2025-09-27 15:15:32 -0600

    fix

--- homes/alex/home.nix
+++ homes/alex/home.nix
@@ -16,6 +16,7 @@
     outputs.homeManagerModules.alex-desktop
     outputs.homeManagerModules.applications
     outputs.homeManagerModules.fonts
+    outputs.homeManagerModules.dependencies
 
     # Or modules exported from other flakes (such as nix-colors):
     # inputs.nix-colors.homeManagerModules.default
