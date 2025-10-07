4b46ed13df44c56955db3c3cc9aed0421b5cb76b
Alex Kumpula <alex.kumpula01@gmail.com>
2025-10-06 20:23:47 -0600

    test fix

--- modules/alex-desktop/mpvpaper/default.nix
+++ modules/alex-desktop/mpvpaper/default.nix
@@ -4,9 +4,9 @@
     enable = true;
     pauseList = ""; # List of applications that pause the wallpaper when running
     stopList = "";  # List of applications that stop the wallpaper when running
-
-    # Workaround for bug: explicitly define the empty files
-    xdg.configFile."mpvpaper/pauselist".text = "";
-    xdg.configFile."mpvpaper/stoplist".text = "";
   };
+
+  # Workaround for bug: explicitly define the empty files
+  xdg.configFile."mpvpaper/pauselist".text = "";
+  xdg.configFile."mpvpaper/stoplist".text = "";
 }
