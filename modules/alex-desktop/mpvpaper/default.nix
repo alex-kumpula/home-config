fec1e8d78c46fa4a837eb997badf40799fef063e
Alex Kumpula <alex.kumpula01@gmail.com>
2025-10-06 20:15:23 -0600

    Fix

--- modules/alex-desktop/mpvpaper/default.nix
+++ modules/alex-desktop/mpvpaper/default.nix
@@ -2,5 +2,7 @@
 {
   programs.mpvpaper = {
     enable = true;
+    pauseList = [ ]; # List of applications that pause the wallpaper when running
+    stopList = [ ];  # List of applications that stop the wallpaper when running
   };
 }
